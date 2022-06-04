e3ac99b17c54a6e1e5ec2b9c6bd3e4e045257151
June <khazoda@gmail.com>
2022-06-04 17:53:15 +0100

    Dwayne slab,stairs added

--- src/main/java/net/dwayne/the/block/johnson/DwayneMod.java
+++ src/main/java/net/dwayne/the/block/johnson/DwayneMod.java
@@ -1,36 +1,55 @@
 package net.dwayne.the.block.johnson;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SlabBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-
 public class DwayneMod implements ModInitializer {
 
-
-    public static final Item DWAYNE_ITEM = new Item(new Item.Settings().group(ItemGroup.MISC));
-
-
     public static SoundEvent DWAYNE_SOUND_EVENT = new SoundEvent(new Identifier("dwayne-the-block-johnson:dwayne"));
-    public static final Block DWAYNE_BLOCK = new DwayneBlock(null,DWAYNE_SOUND_EVENT);
+    public static final Block DWAYNE_BLOCK = new DwayneBlock(null, DWAYNE_SOUND_EVENT);
+    public static final Block DWAYNE_SLAB = new SlabBlock(
+            FabricBlockSettings.copyOf(Blocks.OBSIDIAN).luminance(12).sounds(BlockSoundGroup.AMETHYST_BLOCK)
+                    .hardness(1.5f).requiresTool());
+    public static final Block DWAYNE_STAIRS = new DwayneStairs(DwayneMod.DWAYNE_BLOCK.getDefaultState(), null,
+            DWAYNE_SOUND_EVENT);
+
+    public static final Item DWAYNE_ITEM = new Item(new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
+    // Block Items
+    public static final BlockItem DWAYNE_BLOCK_ITEM = new BlockItem(DWAYNE_BLOCK,
+            new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
+    public static final BlockItem DWAYNE_SLAB_ITEM = new BlockItem(DWAYNE_SLAB,
+            new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
+    public static final BlockItem DWAYNE_STAIRS_ITEM = new BlockItem(DWAYNE_STAIRS,
+            new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
 
     @Override
     public void onInitialize() {
         // Items
-        Registry.register(Registry.ITEM, new Identifier("dwayne-the-block-johnson","dwayne_item"), DWAYNE_ITEM);   
+        Registry.register(Registry.ITEM, new Identifier("dwayne-the-block-johnson", "dwayne_item"), DWAYNE_ITEM);
         // Block Items
-        Registry.register(Registry.ITEM, new Identifier("dwayne-the-block-johnson","dwayne_block"), new BlockItem(DWAYNE_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
+        Registry.register(Registry.ITEM, new Identifier("dwayne-the-block-johnson", "dwayne_block"), DWAYNE_BLOCK_ITEM);
+        Registry.register(Registry.ITEM, new Identifier("dwayne-the-block-johnson", "dwayne_stairs"),
+                DWAYNE_STAIRS_ITEM);
+        Registry.register(Registry.ITEM, new Identifier("dwayne-the-block-johnson", "dwayne_slab"),
+                DWAYNE_SLAB_ITEM);
         // Blocks
-        Registry.register(Registry.BLOCK, new Identifier("dwayne-the-block-johnson","dwayne_block"),DWAYNE_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier("dwayne-the-block-johnson", "dwayne_block"), DWAYNE_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier("dwayne-the-block-johnson", "dwayne_stairs"), DWAYNE_STAIRS);
+        Registry.register(Registry.BLOCK, new Identifier("dwayne-the-block-johnson", "dwayne_slab"), DWAYNE_SLAB);
+
         // Sounds
         Registry.register(Registry.SOUND_EVENT, new Identifier("dwayne-the-block-johnson:dwayne"), DWAYNE_SOUND_EVENT);
 
     }
-    
-    
+
 }
---
+++ src/main/java/net/dwayne/the/block/johnson/DwayneStairs.java
@@ -0,0 +1,66 @@
+package net.dwayne.the.block.johnson;
+
+import java.util.Random;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.StairsBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class DwayneStairs extends StairsBlock {
+
+  public static final BooleanProperty TRIGGERED = Properties.TRIGGERED;
+  public SoundEvent DWAYNE_SOUND_EVENT = null;
+
+  Random rand;
+
+  public DwayneStairs(BlockState baseBlockState, Settings settings, SoundEvent dwayne_sound_event) {
+    super(baseBlockState,
+        FabricBlockSettings.copyOf(Blocks.OBSIDIAN).luminance(12).sounds(BlockSoundGroup.AMETHYST_BLOCK)
+            .hardness(1.5f).requiresTool());
+
+    rand = new Random();
+
+  }
+
+  @Override
+  public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand,
+      BlockHitResult hit) {
+    return ActionResult.CONSUME;
+  }
+
+  @Override
+  public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+    if (!world.isClient) {
+      if (entity.getVelocity().x != 0) {
+        world.playSound(null, pos, SoundEvents.BLOCK_MEDIUM_AMETHYST_BUD_PLACE, SoundCategory.BLOCKS, 1f, 1f);
+      }
+    }
+    // Client Code
+    if (world.isClient) {
+      if (entity.getVelocity().x != 0) {
+        for (int i = 0; i < 10; i++) {
+          world.addParticle(ParticleTypes.END_ROD, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5,
+              rand.nextFloat(-0.15f, 0.15f), rand.nextFloat(-0.15f, 0.15f), rand.nextFloat(-0.15f, 0.15f));
+          world.addParticle(ParticleTypes.GLOW, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5,
+              rand.nextFloat(-0.15f, 0.15f), rand.nextFloat(-0.15f, 0.15f), rand.nextFloat(-0.15f, 0.15f));
+        }
+      }
+    }
+  }
+}
